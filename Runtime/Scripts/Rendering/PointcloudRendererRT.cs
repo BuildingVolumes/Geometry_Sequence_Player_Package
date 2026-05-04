d491b279d7c7d44ddce8c30f3bcc34e4870f2c8b
MrGcGamer <32813275+MrGcGamer@users.noreply.github.com>
2026-05-04 13:21:45 +0200

    Removed unused UnityEditor.Experimental.GraphView references from PointcloudRenderer scripts

--- Runtime/Scripts/Rendering/PointcloudRendererRT.cs
+++ Runtime/Scripts/Rendering/PointcloudRendererRT.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 namespace BuildingVolumes.Player
 {
